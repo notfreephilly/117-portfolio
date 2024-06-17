799a4751d12e92511f117163e2ad496ee318cd4b
notfreephilly <not.philyoder93@gmail.com>
2024-06-17 15:53:11 -0700

    set up email service

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import certifi
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +127,12 @@ STATICFILES_DIR = [str(BASE_DIR.joinpath("static"))]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = "not.philyoder93@gmail.com"
+EMAIL_HOST_PASSWORD = "fsdbcaqqoglwotyh"
+# go back to https://myaccount.google.com/ > app passwords > remove the password
--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import about
+from pages.views import about, home, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", about, name="about"),
+    path("", home, name="home"),
+    path("about", about, name="about"),
+    path("contact", contact, name="contact"),
 ]
